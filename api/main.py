0d36945f2b7291d79b7aca92eb12a9a782c7a3d3
Austin Gailey <austingailey@yahoo.com>
2021-11-30 16:14:53 -0700

    Updating API

--- api/main.py
+++ api/main.py
@@ -4,21 +4,22 @@
 # app = FastAPI()
 
 
-# @app.get("/health", status_code=200)
-# def get_health():
-#     return {"status_code": "200"}
 
-
-# @app.get("/{currency}")
-# async def get_currency(currency):
-#     url = f'https://api.coinbase.com/v2/prices/spot?currency={currency}'
-#     response = requests.get(url)
-#     return response.json()
 from mangum import Mangum
 from fastapi import FastAPI
 
 app = FastAPI()
 
+@app.get("/health", status_code=200)
+def get_health():
+    return {"status_code": "200"}
+
+
+@app.get("/{currency}")
+async def get_currency(currency):
+    url = f'https://api.coinbase.com/v2/prices/spot?currency={currency}'
+    response = requests.get(url)
+    return response.json()
 
 @app.get("/")
 async def root():
