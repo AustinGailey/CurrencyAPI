a40f8768a9edcfdfdb57bf4c01e462f1962164ab
Austin Gailey <austingailey@yahoo.com>
2021-11-30 15:30:37 -0700

    Testing Demo Project

---
+++ api/main.py
@@ -0,0 +1,27 @@
+# from fastapi import FastAPI
+# import requests
+
+# app = FastAPI()
+
+
+# @app.get("/health", status_code=200)
+# def get_health():
+#     return {"status_code": "200"}
+
+
+# @app.get("/{currency}")
+# async def get_currency(currency):
+#     url = f'https://api.coinbase.com/v2/prices/spot?currency={currency}'
+#     response = requests.get(url)
+#     return response.json()
+from mangum import Mangum
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+handler = Mangum(app=app)
--- tests/test_currency.py
+++
@@ -1,9 +0,0 @@
-from fastapi.testclient import TestClient
-from services.currency import app
-
-client = TestClient(app)
-
-def test_root():
-    response = client.get("/health")
-    assert response.status_code == 200
-    assert response.json() ==  {"status_code": "200"} 
