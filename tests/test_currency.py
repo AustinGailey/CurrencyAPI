c60f4a044cd190280bc406517c14665b94cf72d5
Austin Gailey <austingailey@yahoo.com>
2021-11-30 13:48:07 -0700

    Updating with requirements for Github Actionss

---
+++ tests/test_currency.py
@@ -0,0 +1,9 @@
+from fastapi.testclient import TestClient
+from services.currency import app
+
+client = TestClient(app)
+
+def test_root():
+    response = client.get("/health")
+    assert response.status_code == 200
+    assert response.json() ==  {"status_code": "200"}
