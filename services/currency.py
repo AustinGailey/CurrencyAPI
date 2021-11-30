314d1263d886ba317ed5637bb110c9b21a72842d
Austin Gailey <austingailey@yahoo.com>
2021-11-30 11:47:17 -0700

    Adding API and Github Actions

---
+++ services/currency.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+import requests
+
+app = FastAPI()
+
+
+@app.get("/health", status_code=200)
+def get_health():
+    return {"status_code": "200"}
+
+
+@app.get("/{currency}")
+async def get_currency(currency):
+    url = f'https://api.coinbase.com/v2/prices/spot?currency={currency}'
+    response = requests.get(url)
+    return response.json()
