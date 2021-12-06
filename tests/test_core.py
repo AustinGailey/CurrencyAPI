78a4e44c356557f3ae2c0a144ad98bf7f5dd2398
Austin Gailey <austingailey@yahoo.com>
2021-12-05 18:30:13 -0700

    Currency API - COMPLETE

--- tests/test_core.py
+++ tests/test_core.py
@@ -8,10 +8,3 @@ def test_root():
     assert response.status_code == 200
     assert response.json() == {"message": "Currency API"}
 
-def test_health():
-    response = client.get("/health")
-    assert response.status_code == 200
-
-def test_currency():
-    response = client.get("/USD")
-    assert response.json().data.currency == "USD"
